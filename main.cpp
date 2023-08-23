da61c411e30079d88ce63e70f171502b4fb36607
lyj-Desktop <liu.yunjie@qq.com>
2023-08-23 15:56:51 +0800

    Second commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿#include "mainwindow.h"
+
+#include <QApplication>
+#include "semicl6.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    semicl6Initialize();
+    semicl6();
+    return a.exec();
+}
